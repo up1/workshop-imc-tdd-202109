bc0e7d113ae9b1347c1e7dcdcc884605ea3debfe
up1 <somkiat.p@gmail.com>
2021-09-01 15:01:03 +0700

    Add test cases

--- workshop-java-badcode-main/src/test/java/badcode/RegisterBusinessTest.java
+++ workshop-java-badcode-main/src/test/java/badcode/RegisterBusinessTest.java
@@ -19,4 +19,91 @@ public class RegisterBusinessTest {
         assertEquals("First name is required.", exception.getMessage());
     }
 
+    @Test
+    @DisplayName("Throw exception with First name is required.")
+    public void case02() {
+        // Arrange
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        // Act and Assert
+        Exception exception
+                = assertThrows(ArgumentNullException.class, () ->
+        {
+            Speaker speaker = new Speaker();
+            speaker.setFirstName("Somkiat");
+            registerBusiness.register(null, speaker);
+        });
+        assertEquals("Last name is required.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Throw exception with Email is required..")
+    public void case03() {
+        // Arrange
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        // Act and Assert
+        Exception exception
+                = assertThrows(ArgumentNullException.class, () ->
+        {
+            Speaker speaker = new Speaker();
+            speaker.setFirstName("Somkiat");
+            speaker.setLastName("Pui");
+            registerBusiness.register(null, speaker);
+        });
+        assertEquals("Email is required.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Throw exception with DomainEmailInvalidException.")
+    public void case04() {
+        // Arrange
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        // Act and Assert
+        Exception exception
+                = assertThrows(DomainEmailInvalidException.class, () ->
+        {
+            Speaker speaker = new Speaker();
+            speaker.setFirstName("Somkiat");
+            speaker.setLastName("Pui");
+            speaker.setEmail("somkiat");
+            registerBusiness.register(null, speaker);
+        });
+    }
+
+    @Test
+    @DisplayName("Throw exception with Speaker doesn't meet our standard rules.")
+    public void case05() {
+        // Arrange
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        // Act and Assert
+        Exception exception
+                = assertThrows(SpeakerDoesntMeetRequirementsException.class, () ->
+        {
+            Speaker speaker = new Speaker();
+            speaker.setFirstName("Somkiat");
+            speaker.setLastName("Pui");
+            speaker.setEmail("somkiat@xxx.com");
+            registerBusiness.register(null, speaker);
+        });
+
+        assertEquals("Speaker doesn't meet our standard rules.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Throw exception with Can't save a speaker.")
+    public void case06() {
+        // Arrange
+        RegisterBusiness registerBusiness = new RegisterBusiness();
+        // Act and Assert
+        Exception exception
+                = assertThrows(SaveSpeakerException.class, () ->
+        {
+            Speaker speaker = new Speaker();
+            speaker.setFirstName("Somkiat");
+            speaker.setLastName("Pui");
+            speaker.setEmail("somkiat@gmail.com");
+            registerBusiness.register(null, speaker);
+        });
+
+        assertEquals("Can't save a speaker.", exception.getMessage());
+    }
 }
