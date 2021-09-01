5d2fe90e3b0a426d7390cdd5064a0d8468461f5f
up1 <somkiat.p@gmail.com>
2021-09-01 11:17:16 +0700

    Start with range workshop

---
+++ day01/src/test/java/MyRangeTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MyRangeTest {
+
+    @Test
+    @DisplayName("")
+    public void case01() {
+        // Arrange
+        MyRange myRange = new MyRange();
+        // TODO
+        // Act
+        // Assert
+    }
+
+}
