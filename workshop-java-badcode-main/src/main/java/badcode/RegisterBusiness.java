ed4f7e53818f8582a5c73de0aa2c8caf7b243986
up1 <somkiat.p@gmail.com>
2021-09-01 15:48:51 +0700

    Move logic

--- workshop-java-badcode-main/src/main/java/badcode/RegisterBusiness.java
+++ workshop-java-badcode-main/src/main/java/badcode/RegisterBusiness.java
@@ -7,7 +7,6 @@ public class RegisterBusiness {
     public Integer register(SpeakerRepository repository, Speaker speaker) {
         Integer speakerId;
         validateInput(speaker);
-        speaker.setRegistrationFee(getFee(speaker.getExp()));
         try {
             speakerId = repository.saveSpeaker(speaker);
         } catch (Exception exception) {
@@ -39,20 +38,6 @@ public class RegisterBusiness {
 
     private boolean isNullOrEmpty(String firstName) {
         return firstName == null || firstName.trim().equals("");
-    }
-
-    int getFee(int experienceYear) {
-        int fee = 0;
-        if (experienceYear <= 1) {
-            fee = 500;
-        } else if (experienceYear <= 3) {
-            fee = 250;
-        } else if (experienceYear <= 5) {
-            fee = 100;
-        } else if (experienceYear <= 9) {
-            fee = 50;
-        }
-        return fee;
     }
 
     public String getEmailDomain(String email) {
