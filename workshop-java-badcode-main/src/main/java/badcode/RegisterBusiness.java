946cf11731270df9cc9c40831b9d22a7b8de09fc
up1 <somkiat.p@gmail.com>
2021-09-01 15:32:58 +0700

    Reduce complex if

--- workshop-java-badcode-main/src/main/java/badcode/RegisterBusiness.java
+++ workshop-java-badcode-main/src/main/java/badcode/RegisterBusiness.java
@@ -8,30 +8,27 @@ public class RegisterBusiness {
         Integer speakerId;
         String[] domains = {"gmail.com", "live.com"};
 
-        if (speaker.getFirstName() != null && !speaker.getFirstName().trim().equals("")) {
-            if (speaker.getLastName() != null && !speaker.getLastName().trim().equals("")) {
-                if (speaker.getEmail() != null && !speaker.getEmail().trim().equals("")) {
-                    // Your Tasks ...
-                    String emailDomain = getEmailDomain(speaker.getEmail()); // Avoid ArrayIndexOutOfBound
-                    if (Arrays.stream(domains).filter(it -> it.equals(emailDomain)).count() == 1) {
-                        int exp = speaker.getExp();
-                        speaker.setRegistrationFee(getFee(exp));
-                        try {
-                            speakerId = repository.saveSpeaker(speaker);
-                        } catch (Exception exception) {
-                            throw new SaveSpeakerException("Can't save a speaker.");
-                        }
-                    } else {
-                        throw new SpeakerDoesntMeetRequirementsException("Speaker doesn't meet our standard rules.");
-                    }
-                } else {
-                    throw new ArgumentNullException("Email is required.");
-                }
-            } else {
-                throw new ArgumentNullException("Last name is required.");
+        if (speaker.getFirstName() == null || speaker.getFirstName().trim().equals("")) {
+            throw new ArgumentNullException("First name is required.");
+        }
+        if (speaker.getLastName() == null || speaker.getLastName().trim().equals("")) {
+            throw new ArgumentNullException("Last name is required.");
+        }
+        if (speaker.getEmail() == null || speaker.getEmail().trim().equals("")) {
+            throw new ArgumentNullException("Email is required.");
+        }
+        // Your Tasks ...
+        String emailDomain = getEmailDomain(speaker.getEmail()); // Avoid ArrayIndexOutOfBound
+        if (Arrays.stream(domains).filter(it -> it.equals(emailDomain)).count() == 1) {
+            int exp = speaker.getExp();
+            speaker.setRegistrationFee(getFee(exp));
+            try {
+                speakerId = repository.saveSpeaker(speaker);
+            } catch (Exception exception) {
+                throw new SaveSpeakerException("Can't save a speaker.");
             }
         } else {
-            throw new ArgumentNullException("First name is required.");
+            throw new SpeakerDoesntMeetRequirementsException("Speaker doesn't meet our standard rules.");
         }
 
         return speakerId;
