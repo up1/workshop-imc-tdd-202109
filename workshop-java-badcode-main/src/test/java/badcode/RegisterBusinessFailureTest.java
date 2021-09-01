3a52b45019029e832e6e40b7ebf82ade08834f73
up1 <somkiat.p@gmail.com>
2021-09-01 15:15:22 +0700

    Rename file

--- workshop-java-badcode-main/src/test/java/badcode/RegisterBusinessFailureTest.java
+++ workshop-java-badcode-main/src/test/java/badcode/RegisterBusinessFailureTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class RegisterBusinessTest {
+public class RegisterBusinessFailureTest {
 
     @Test
     @DisplayName("Throw exception with First name is required.")
