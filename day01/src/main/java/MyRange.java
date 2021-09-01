b7c9b348f1206f55fd75b4728d7bdf2ec12b00db
up1 <somkiat.p@gmail.com>
2021-09-01 11:35:39 +0700

    Start with include case

--- day01/src/main/java/MyRange.java
+++ day01/src/main/java/MyRange.java
@@ -1,2 +1,11 @@
 public class MyRange {
+    private final String input;
+
+    public MyRange(String input) {
+        this.input = input;
+    }
+
+    public boolean startWithInclude() {
+        return this.input.startsWith("[");
+    }
 }
--- day01/src/test/java/MyRangeTest.java
+++ day01/src/test/java/MyRangeTest.java
@@ -1,16 +1,19 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MyRangeTest {
 
     @Test
-    @DisplayName("")
+    @DisplayName("Start with include([)")
     public void case01() {
         // Arrange
-        MyRange myRange = new MyRange();
-        // TODO
+        MyRange myRange = new MyRange("[1,5]");
         // Act
+        boolean expectedResult = myRange.startWithInclude();
         // Assert
+        assertTrue(expectedResult);
     }
 
 }
