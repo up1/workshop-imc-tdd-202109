bbdf91466514178994b30e295b3c480793ad3f69
up1 <somkiat.p@gmail.com>
2021-09-02 14:31:44 +0700

    Repository testing :: success case

--- myapi/src/test/java/com/example/myapi/product/ProductRepositoryTest.java
+++ myapi/src/test/java/com/example/myapi/product/ProductRepositoryTest.java
@@ -15,8 +15,18 @@ public class ProductRepositoryTest {
 
     @Test
     @DisplayName("ดึงข้อมูลของ product id = 1 ได้")
-    public void caseo1() {
-
+    public void case1() {
+        // Arrange
+        Product newProduct = new Product();
+        newProduct.setName("demo name");
+        newProduct.setPrice(100);
+        productRepository.save(newProduct);
+        // Act
+        Product product1 = productRepository.getById(1);
+        // Assert
+        assertEquals(1, product1.getId());
+        assertEquals("demo name", product1.getName());
+        assertEquals(100, product1.getPrice());
     }
 
 }
