4d512c4ce50a85d1e5260fe22a8481d8a525aa41
up1 <somkiat.p@gmail.com>
2021-09-02 15:29:31 +0700

    Use post construct to initial data for testing

--- myapi/src/main/java/com/example/myapi/MyapiApplication.java
+++ myapi/src/main/java/com/example/myapi/MyapiApplication.java
@@ -1,14 +1,28 @@
 package com.example.myapi;
 
+import com.example.myapi.product.Product;
+import com.example.myapi.product.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import javax.annotation.PostConstruct;
 import java.util.Arrays;
 
 
 @SpringBootApplication
 public class MyapiApplication {
+
+	@Autowired ProductRepository productRepository;
+
+	@PostConstruct
+	public void initialData() {
+		Product newProduct = new Product();
+		newProduct.setName("พัดลม");
+		newProduct.setPrice(1200);
+		productRepository.save(newProduct);
+	}
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(MyapiApplication.class, args);
