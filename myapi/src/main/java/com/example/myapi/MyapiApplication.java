3131f1de9fe226c9f862b25a391ddd013b91050b
up1 <somkiat.p@gmail.com>
2021-09-02 10:07:21 +0700

    Start with spring boot project

---
+++ myapi/src/main/java/com/example/myapi/MyapiApplication.java
@@ -0,0 +1,21 @@
+package com.example.myapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class MyapiApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(MyapiApplication.class, args);
+		String[] names = context.getBeanDefinitionNames();
+		Arrays.stream(names).forEach(System.out::println);
+
+		int count = context.getBeanDefinitionCount();
+		System.out.println("All beans = " + count);
+	}
+
+}
