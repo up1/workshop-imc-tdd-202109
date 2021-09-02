e14fed524538ea4929664a56a6d7b1fc16d56ff5
up1 <somkiat.p@gmail.com>
2021-09-02 13:41:26 +0700

    Working with MVC Test

--- myapi/src/test/java/com/example/myapi/product/ProductControllerWebMvcTest.java
+++ myapi/src/test/java/com/example/myapi/product/ProductControllerWebMvcTest.java
@@ -1,10 +1,14 @@
 package com.example.myapi.product;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -16,8 +20,36 @@ public class ProductControllerWebMvcTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private ProductService productService;
+
     @Test
     public void getProductDetail() throws Exception {
-        mvc.perform(get("/products/1")).andExpect(status().isOk());
+        // Arrange
+        ProductResponse mockResult = new ProductResponse();
+        mockResult.setId(1000);
+        mockResult.setProductName("ทดสอบ");
+        mockResult.setPrice(3000);
+        when(productService.getById(1)).thenReturn(mockResult);
+
+        // Act
+        MvcResult result =
+                mvc.perform(get("/products/1")).andExpect(status().isOk())
+                        .andReturn();
+        String resultJson = result.getResponse().getContentAsString();
+        // Convert from string json to java object
+        ObjectMapper mapper = new ObjectMapper();
+        ProductResponse productResponse =
+                mapper.readValue(resultJson, ProductResponse.class);
+        // Assert
+        ProductResponse expected = new ProductResponse();
+        expected.setId(1000);
+        expected.setProductName("ทดสอบ");
+        expected.setPrice(3000);
+
+        assertEquals(1000, productResponse.getId());
+        assertEquals(3000, productResponse.getPrice());
+        assertEquals("ทดสอบ", productResponse.getProductName());
+
     }
 }
