c84aff65b07acdc44e7fed6a23fb762fcb87d2fc
up1 <somkiat.p@gmail.com>
2021-09-02 17:18:18 +0700

    Create mock api with stubby

--- myapi/src/main/java/com/example/myapi/post/PostGateway.java
+++ myapi/src/main/java/com/example/myapi/post/PostGateway.java
@@ -1,6 +1,7 @@
 package com.example.myapi.post;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -13,9 +14,12 @@ public class PostGateway {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${post_url}")
+    private String postUrl;
+
     // https://jsonplaceholder.typicode.com/posts/1
     public Optional<PostResponse> getPostById(int id) {
-        String url = "https://jsonplaceholder.typicode.com/posts/" + id;
+        String url = postUrl + "/posts/" + id;
         try {
             PostResponse response =
                     restTemplate.getForObject(url, PostResponse.class);
