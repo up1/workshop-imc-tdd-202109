322bfce604fa95312d57c9cd7203da77de001d89
up1 <somkiat.p@gmail.com>
2021-09-02 11:29:48 +0700

    Compare object in java

--- myapi/src/test/java/com/example/myapi/product/ProductControllerSpringBootTest.java
+++ myapi/src/test/java/com/example/myapi/product/ProductControllerSpringBootTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.context.SpringBootTest.*;
-
+// 1. Start server
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ProductControllerSpringBootTest {
 
@@ -23,5 +23,11 @@ public class ProductControllerSpringBootTest {
         assertEquals(1, productResponse.getId());
         assertEquals("พัดลม", productResponse.getProductName());
         assertEquals(1200, productResponse.getPrice());
+
+        ProductResponse expected = new ProductResponse();
+        expected.setId(1);
+        expected.setProductName("พัดลม");
+        expected.setPrice(1200);
+        assertEquals(expected, productResponse);
     }
 }
