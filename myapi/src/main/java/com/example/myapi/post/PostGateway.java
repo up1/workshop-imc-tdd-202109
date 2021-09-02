17b38acafbce536ce7a7cc5806cd7f9051137400
up1 <somkiat.p@gmail.com>
2021-09-02 16:50:39 +0700

    Working with resttemplate

--- myapi/src/main/java/com/example/myapi/post/PostGateway.java
+++ myapi/src/main/java/com/example/myapi/post/PostGateway.java
@@ -1,13 +1,29 @@
 package com.example.myapi.post;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @Component
 public class PostGateway {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     // https://jsonplaceholder.typicode.com/posts/1
-    public PostResponse getPostById(int id) {
-        return null;
+    public Optional<PostResponse> getPostById(int id) {
+        String url = "https://jsonplaceholder.typicode.com/posts/" + id;
+        try {
+            PostResponse response = restTemplate.getForObject(url, PostResponse.class);
+            return Optional.ofNullable(response);
+        }catch (RestClientException e) {
+            return Optional.empty();
+        }
     }
 
 }
+
+// RestTemplate, Feign/ OpenFeign, Retrofit
